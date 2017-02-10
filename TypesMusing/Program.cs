119c122bf582e7b0f6fc5728f65c46c84d9ac2d2
Alex Dorokhin <adorokhin@hotmail.com>
2017-02-10 15:49:49 -0500

    Three

--- TypesMusing/Program.cs
+++ TypesMusing/Program.cs
@@ -19,6 +19,36 @@ namespace ConsoleApplication
         {
             s.a.speak();
         }
+
+        #region [Casting]
+        public class Animal
+        {
+            public Animal() { }
+            public Animal(string anymalSays) { this.anymalSays = anymalSays; }
+            public virtual string WhoAmI() { return this.GetType().ToString(); }
+
+            public string anymalSays = "#@!$&";
+        }
+        public class Dog : Animal
+        {
+            public Dog(string anymalSpeaks) : base(anymalSpeaks) { }
+
+            public static explicit operator Dog(Cat c)
+            {
+                return new Dog(c.anymalSays);
+            }
+        }
+        public class Cat : Animal
+        {
+            public Cat(string anymalSpeaks) : base(anymalSpeaks) { }
+
+            public static implicit operator Cat(Dog dog)
+            {
+                return new Cat(dog.anymalSays);
+            }
+        }
+        #endregion
+
         public static void Main(string[] args)
         {
             #region [Uses for different types]
@@ -85,30 +115,148 @@ namespace ConsoleApplication
             {
                 A a;
                 A[] arr = new A[10];
-                foreach (var aa in arr)
-                {
-                    System.Console.WriteLine(aa.speak());
-                }
+                //foreach (var aa in arr)
+                //{
+                //    System.Console.WriteLine(aa.speak());
+                //}
 
                 Str str;
-                str.a = null;
-                useStruct(str);
+                //str.a = null;
+                //useStruct(str);
 
-                Str str1 = new Str();
-                useStruct(str1);
+                //Str str1 = new Str();
+                //useStruct(str1);
 
                 string s = "string";
                 s.ToUpper();
+
+                double d = 1 / 7; //0.(142857)
             }
             #endregion
 
-            #region [defaults]
-            object obj = default(decimal);
+            #region [Casting]
+            {
+
+                //implicit
+                int i = int.MaxValue;
+                long l = i;
+
+
+                //explicit
+                i = -1;
+                uint ui = 0;
+                ui = (uint)i;
+
+                float f = 0;
+                f = (float)double.MaxValue;
+
+                f = float.MaxValue + 1;
+                if(float.IsInfinity(f))
+                    Console.WriteLine("To Infinity and Beyond");
+
+
+
+
+                try
+                {
+                    Dog dog = new Dog("Woof-Woof!");
+                    Cat cat = new Cat("Mew?");
+                    System.Console.WriteLine($"Dog says: [{dog.anymalSays}]    {dog.GetType()}");
+                    System.Console.WriteLine($"Cat says: [{cat.anymalSays}]    {cat.GetType()}");
+                    System.Console.WriteLine("Explicitly converting Cat to Dog");
+
+                    dog = (Dog)cat;
+                    System.Console.WriteLine($"Dog says: [{dog.anymalSays}]    {dog.GetType()}");
+
+
+                    dog = new Dog("Woof-Woof!");
+                    cat = new Cat("Mew?");
+                    System.Console.WriteLine($"Dog says: [{dog.anymalSays}]    {dog.GetType()}");
+                    System.Console.WriteLine($"Cat says: [{cat.anymalSays}]    {cat.GetType()}");
+                    System.Console.WriteLine("IMPLICITLY converting Dog to Cat");
+
+                    cat = (Cat)dog;
+                    System.Console.WriteLine($"Cat says: [{cat.anymalSays}]    {cat.GetType()}");
+                }
+                catch (System.Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    System.Console.WriteLine(ex.ToString());
+                    Console.ResetColor();
+                }
+            }
+            #endregion
+
+            #region [Overflows]
+            {
+                int i;
+                int iMax = int.MaxValue;
+                //i = int.MaxValue + 1; //compiler got your back
+                i = iMax + 1; //compiler does not care
+
+                unchecked
+                {
+                    i = int.MaxValue + 1;
+                }
+
+                i = unchecked(int.MaxValue + 1);
+
+                try
+                {
+                    checked { i = iMax + 1; }
+                    i = checked(iMax + 1);
+                }
+                catch (Exception) { }
+
+                
+                try
+                {
+                    uint ui = uint.MaxValue;
+                    uint uiMax = uint.MaxValue;
+                    ui += 1;
+                    ui = checked(uiMax + 1);
+                }
+                catch (Exception) { }
+
+            }
+            #endregion
+
+            #region [Pointers]
+            {
+                //stolen from MSDN
+                // Normal pointer to an object.  
+                int[] a = new int[5] { 10, 20, 30, 40, 50 };
+                // Must be in unsafe code to use interior pointers.  
+                unsafe
+                {
+                    // Must pin object on heap so that it doesn't move while using interior pointers.  
+                    fixed (int* p = &a[0])
+                    {
+                        // p is pinned as well as object, so create another pointer to show incrementing it.  
+                        int* p2 = p;
+                        Console.WriteLine(*p2);
+                        // Incrementing p2 bumps the pointer by four bytes due to its type ...  
+                        p2 += 1;
+                        Console.WriteLine(*p2);
+                        p2 += 1;
+                        Console.WriteLine(*p2);
+                        Console.WriteLine("--------");
+                        Console.WriteLine(*p);
+                        // Deferencing p and incrementing changes the value of a[0] ...  
+                        *p += 1;
+                        Console.WriteLine(*p);
+                        *p += 1;
+                        Console.WriteLine(*p);
+                    }
+                }
+
+                Console.WriteLine("--------");
+                Console.WriteLine(a[0]);
+            }
             #endregion
 
             System.Console.WriteLine("Thats All Folks!");
             System.Console.ReadLine();
-
         }
     }
 }
